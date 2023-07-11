831329840afb29332b955ca305dfa47ea2d9b7e7
ylu8888 <yang.lu.3@stonybrook.edu>
2023-07-11 01:31:18 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import MovieList from './components/MovieList';
+import Heading from './components/Heading';
+import Search from './components/Search';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const key = '7283eef1';
+
+  //movies, search, query state variables
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  const [favorites, setFavorites] = useState([]);
+
+  //async operation to reduce overall processing time by running other parts of code while API fetches data
+ const getMovies = async () => { //await pauses this function while data is fetched.
+  const response = await fetch(`https://www.omdbapi.com/?s=${search}&apikey=${key}`);
+  const data = await response.json(); //turn response to json and store in data
+
+  if(data.Search){ // "Search" is an Omdb API array that true if a query matches any movies
+    setMovies(data.Search); //movie data is stored in movies state
+    
+  }
+   
+ };
+
+ //useEffect hook always gets called on the first render when the page loads
+//when the search state changes, useEffect hook runs getMovies to fetch from API
+useEffect(() => { 
+  getMovies(search);
+}, [search]);
+ 
+ 
+
+ return(
+  <div className='container-fluid movie-app'>
+    <div className='row d-flex align-items-center mt-4 mb-4'>
+      <Heading heading="watch2day"/>
+      <Search search={search} setSearch={setSearch}/>
     </div>
-  );
+    <div className='row'>
+      <MovieList movies = {movies}/>
+    </div>
+    
+
+  </div>
+ );
+
 }
+
 
 export default App;
